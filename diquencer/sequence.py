43dd8d22955ed72a31afe7d51c575aae5c38498b
Maciej Lenartowicz <mcjlnrtwcz@gmail.com>
2018-11-20 21:33:59 +0100

    Creating sequence from raw data is optional.

--- diquencer/sequence.py
+++ diquencer/sequence.py
@@ -6,41 +6,56 @@ from .models import Pattern, PatternSequence
 class Sequence:
     # TODO: next() returns a Pattern (use repetitions from PatternSequence)
 
-    def __init__(self, tempo, sequence):
+    def __init__(self, tempo=120, pattern_seqs=[]):
         self.tempo = tempo
-        self._patterns_blueprint = []
-        self._patterns = []
+        self._pattern_seqs = pattern_seqs
+        self._patterns_blueprint = deepcopy(self._pattern_seqs)  # For reset
+        self._stop_event = 0
 
+        # Get pulsestamp of stop event
+        if self._pattern_seqs:
+            last_pattern_seq = self._pattern_seqs[-1]
+            self._stop_event = last_pattern_seq.pulsestamp
+            self._stop_event += (
+                last_pattern_seq.pattern.length
+                * last_pattern_seq.repetitions
+                * 24 * 4
+            )
+
+    @classmethod
+    def from_raw_data(cls, raw_data):
+        pattern_seqs = []
         last_event_pulses = 0
 
-        for index, pattern_sequence in enumerate(sequence):
+        for index, pattern_sequence in enumerate(raw_data['sequence']):
             pattern = Pattern(
                 pattern_sequence['pattern'],
                 pattern_sequence['bank'],
                 pattern_sequence['length']
             )
             if index != 0:
-                last_event_pulses += (pattern_sequence['length']
-                                      * pattern_sequence['repetitions']
-                                      * 24 * 4)
-            pattern_seq = PatternSequence(pattern, last_event_pulses)
-            self._patterns_blueprint.append(pattern_seq)
+                last_event_pulses += (
+                    pattern.length
+                    * pattern_sequence['repetitions']
+                    * 24 * 4
+                )
+            pattern_seq = PatternSequence(
+                pattern,
+                pattern_sequence['repetitions'],
+                last_event_pulses
+            )
+            pattern_seqs.append(pattern_seq)
 
-        self._patterns = deepcopy(self._patterns_blueprint)  # For reset
-
-        self._stop_event = last_event_pulses
-        self._stop_event += (sequence[-1]['length']
-                             * sequence[-1]['repetitions']
-                             * 24 * 4)
+        return cls(raw_data['tempo'], pattern_seqs)
 
     def get_event(self, pulsestamp):
-        if len(self._patterns) > 0:
-            next_pattern_sequence = self._patterns[0]
+        if len(self._pattern_seqs) > 0:
+            next_pattern_sequence = self._pattern_seqs[0]
             if next_pattern_sequence.pulsestamp - 24 <= pulsestamp:
-                return self._patterns.pop(0).pattern
+                return self._pattern_seqs.pop(0).pattern
         else:
             if pulsestamp == self._stop_event:
                 return 'stop'
 
     def reset(self):
-        self._patterns = deepcopy(self._patterns_blueprint)
+        self._pattern_seqs = deepcopy(self._patterns_blueprint)
